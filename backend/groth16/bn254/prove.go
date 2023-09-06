22818bd7d13ec975ee266ec8425767352fa08e09
liuxiaobleach <1241368737@qq.com>
2023-09-06 17:08:01 +0800

    update

--- backend/groth16/bn254/prove.go
+++ backend/groth16/bn254/prove.go
@@ -244,14 +244,14 @@ func Prove(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, opts ...b
 		// filter the wire values if needed;
 		_wireValues := filter(wireValues, r1cs.CommitmentInfo.PrivateToPublic())
 
-		/*scals := _wireValues[r1cs.GetNbPublicVariables():]
+		scals := _wireValues[r1cs.GetNbPublicVariables():]
 
 		// Filter scalars matching infinity point indices
 		for _, indexToRemove := range pk.G1InfPointIndices.K {
 			scals = append(scals[:indexToRemove], scals[indexToRemove+1:]...)
 		}
 
-		scalarBytes := len(scals) * fr.Bytes
+		/*scalarBytes := len(scals) * fr.Bytes
 		scalars_d, _ := goicicle.CudaMalloc(scalarBytes)
 		goicicle.CudaMemCpyHtoD[fr.Element](scalars_d, scals, scalarBytes)
 		MontConvOnDevice(scalars_d, len(scals), false)*/
