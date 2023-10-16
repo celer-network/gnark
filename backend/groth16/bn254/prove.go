4cbf0e42785605d5d6f8110d878a3cd2fc0544ca
liuxiaobleach <1241368737@qq.com>
2023-10-16 16:13:02 +0800

    test best speed

--- backend/groth16/bn254/prove.go
+++ backend/groth16/bn254/prove.go
@@ -137,7 +137,10 @@ func SolveAndCompress(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness
 		return nil, nil, err
 	}
 	var bufCompressed bytes.Buffer
-	writer := gzip.NewWriter(&bufCompressed)
+	writer, err := gzip.NewWriterLevel(&bufCompressed, gzip.BestSpeed)\
+	if err != nil {
+		return nil, nil, err
+	}
 	_, err = writer.Write(buf.Bytes())
 	if err != nil {
 		return nil, nil, err
