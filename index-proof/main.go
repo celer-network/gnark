eaac1655f8542a041f326d7ff1ead7cb800a0470
王安然 <grr@grr.local>
2023-10-19 16:21:16 +0800

    update 377

--- index-proof/main.go
+++ index-proof/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"log"
-	"os"
-
-	"github.com/consensys/gnark/backend/groth16"
+	// "github.com/consensys/gnark/backend/groth16"
 )
 
 func main() {
@@ -79,48 +77,48 @@ func main() {
 	// }
 }
 
-func WriteProvingKey(pk groth16.ProvingKey, filename string) error {
-	f, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	_, err = pk.WriteTo(f)
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// func WriteProvingKey(pk groth16.ProvingKey, filename string) error {
+// 	f, err := os.Create(filename)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	_, err = pk.WriteTo(f)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
 
-func ReadProvingKey(filename string, pk groth16.ProvingKey) error {
-	f, err := os.Open(filename)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
+// func ReadProvingKey(filename string, pk groth16.ProvingKey) error {
+// 	f, err := os.Open(filename)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer f.Close()
 
-	_, err = pk.UnsafeReadFrom(f)
-	return err
-}
+// 	_, err = pk.UnsafeReadFrom(f)
+// 	return err
+// }
 
-func WriteVerifyingKey(vk groth16.VerifyingKey, filename string) error {
-	f, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
+// func WriteVerifyingKey(vk groth16.VerifyingKey, filename string) error {
+// 	f, err := os.Create(filename)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	_, err = vk.WriteTo(f)
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// 	_, err = vk.WriteTo(f)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
 
-func ReadVerifyingKey(filename string, vk groth16.VerifyingKey) error {
-	f, _ := os.Open(filename)
-	defer f.Close()
-	_, err := vk.ReadFrom(f)
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// func ReadVerifyingKey(filename string, vk groth16.VerifyingKey) error {
+// 	f, _ := os.Open(filename)
+// 	defer f.Close()
+// 	_, err := vk.ReadFrom(f)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
