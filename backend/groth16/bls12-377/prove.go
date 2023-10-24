bd7d017ddfdebbaf4d83acc4d365a6a2420c3f54
王安然 <grr@grr.local>
2023-10-24 14:17:55 +0800

    test 377

--- backend/groth16/bls12-377/prove.go
+++ backend/groth16/bls12-377/prove.go
@@ -184,61 +184,61 @@ func Prove(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, opts ...b
 		return nil
 	}
 
-	computeKRS := func() {
-		// we could NOT split the Krs multiExp in 2, and just append pk.G1.K and pk.G1.Z
-		// however, having similar lengths for our tasks helps with parallelism
-
-		var krs, krs2, p1 curve.G1Jac
-		sizeH := int(pk.Domain.Cardinality - 1) // comes from the fact the deg(H)=(n-1)+(n-1)-n=n-2
-
-		icicleRes, _, _, timing := MsmOnDevice2(h, pk.G1Device.Z, sizeH, BUCKET_FACTOR, true)
-		log.Debug().Dur("took", timing).Msg("Icicle API: MSM KRS2 MSM")
-
-		krs2 = icicleRes
-		// filter the wire values if needed;
-		_wireValues := filter(wireValues, r1cs.CommitmentInfo.PrivateToPublic())
-
-		scals := _wireValues[r1cs.GetNbPublicVariables():]
-
-		// Filter scalars matching infinity point indices
-		for _, indexToRemove := range pk.G1InfPointIndices.K {
-			scals = append(scals[:indexToRemove], scals[indexToRemove+1:]...)
-		}
-
-		scalarBytes := len(scals) * fr.Bytes
-		scalars_d, _ := goicicle.CudaMalloc(scalarBytes)
-		goicicle.CudaMemCpyHtoD[fr.Element](scalars_d, scals, scalarBytes)
-		MontConvOnDevice(scalars_d, len(scals), false)
-
-		icicleRes, _, _, timing = MsmOnDevice2(scalars_d, pk.G1Device.K, len(scals), BUCKET_FACTOR, true)
-		log.Debug().Dur("took", timing).Msg("Icicle API: MSM KRS MSM")
-
-		goicicle.CudaFree(scalars_d)
-
-		krs = icicleRes
-		krs.AddMixed(&deltas[2])
-
-		krs.AddAssign(&krs2)
-
-		p1.ScalarMultiplication(*&ar, *&s)
-		krs.AddAssign(&p1)
-
-		p1.ScalarMultiplication(*&bs1, *&r)
-		krs.AddAssign(&p1)
-
-		proof.Krs.FromJacobian(&krs)
-	}
-
-	// computeKRS := func() error {
+	// computeKRS := func() {
 	// 	// we could NOT split the Krs multiExp in 2, and just append pk.G1.K and pk.G1.Z
 	// 	// however, having similar lengths for our tasks helps with parallelism
-	// 	krsErr := KrsMsmOnDevice(h, pk.G1Device.Z, pk.G1Device.K, pk.Domain.Cardinality, wireValues,
-	// 		r1cs.CommitmentInfo.PrivateToPublic(), pk.G1InfPointIndices.K, r1cs.GetNbPublicVariables(), &deltas[2], &proof.Krs, ar, bs1, s, r)
-	// 	if krsErr != nil {
-	// 		return krsErr
+
+	// 	var krs, krs2, p1 curve.G1Jac
+	// 	sizeH := int(pk.Domain.Cardinality - 1) // comes from the fact the deg(H)=(n-1)+(n-1)-n=n-2
+
+	// 	icicleRes, _, _, timing := MsmOnDevice2(h, pk.G1Device.Z, sizeH, BUCKET_FACTOR, true)
+	// 	log.Debug().Dur("took", timing).Msg("Icicle API: MSM KRS2 MSM")
+
+	// 	krs2 = icicleRes
+	// 	// filter the wire values if needed;
+	// 	_wireValues := filter(wireValues, r1cs.CommitmentInfo.PrivateToPublic())
+
+	// 	scals := _wireValues[r1cs.GetNbPublicVariables():]
+
+	// 	// Filter scalars matching infinity point indices
+	// 	for _, indexToRemove := range pk.G1InfPointIndices.K {
+	// 		scals = append(scals[:indexToRemove], scals[indexToRemove+1:]...)
 	// 	}
-	// 	return nil
+
+	// 	scalarBytes := len(scals) * fr.Bytes
+	// 	scalars_d, _ := goicicle.CudaMalloc(scalarBytes)
+	// 	goicicle.CudaMemCpyHtoD[fr.Element](scalars_d, scals, scalarBytes)
+	// 	MontConvOnDevice(scalars_d, len(scals), false)
+
+	// 	icicleRes, _, _, timing = MsmOnDevice2(scalars_d, pk.G1Device.K, len(scals), BUCKET_FACTOR, true)
+	// 	log.Debug().Dur("took", timing).Msg("Icicle API: MSM KRS MSM")
+
+	// 	goicicle.CudaFree(scalars_d)
+
+	// 	krs = icicleRes
+	// 	krs.AddMixed(&deltas[2])
+
+	// 	krs.AddAssign(&krs2)
+
+	// 	p1.ScalarMultiplication(*&ar, *&s)
+	// 	krs.AddAssign(&p1)
+
+	// 	p1.ScalarMultiplication(*&bs1, *&r)
+	// 	krs.AddAssign(&p1)
+
+	// 	proof.Krs.FromJacobian(&krs)
 	// }
+
+	computeKRS := func() error {
+		// we could NOT split the Krs multiExp in 2, and just append pk.G1.K and pk.G1.Z
+		// however, having similar lengths for our tasks helps with parallelism
+		krsErr := KrsMsmOnDevice(h, pk.G1Device.Z, pk.G1Device.K, pk.Domain.Cardinality, wireValues,
+			r1cs.CommitmentInfo.PrivateToPublic(), pk.G1InfPointIndices.K, r1cs.GetNbPublicVariables(), &deltas[2], &proof.Krs, ar, bs1, s, r)
+		if krsErr != nil {
+			return krsErr
+		}
+		return nil
+	}
 
 	computeBS2 := func() error {
 		// Bs2 (1 multi exp G2 - size = len(wires))
