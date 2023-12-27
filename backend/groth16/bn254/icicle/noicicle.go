1af370939d0f20f964a9e8b2acf45d1b53158c86
王安然 <grr@grr.lan>
2023-12-27 15:24:57 +0800

    update

--- backend/groth16/bn254/icicle/noicicle.go
+++ backend/groth16/bn254/icicle/noicicle.go
@@ -11,7 +11,7 @@ import (
 	cs "github.com/consensys/gnark/constraint/bn254"
 )
 
-const HasIcicle = true
+const HasIcicle = false
 
 func Prove(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, opts ...backend.ProverOption) (*groth16_bn254.Proof, error) {
 	return nil, fmt.Errorf("icicle backend requested but program compiled without 'icicle' build tag")
