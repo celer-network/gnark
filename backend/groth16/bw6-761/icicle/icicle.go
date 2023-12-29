84092f2c845242122546ef329a46910d65312f1a
liuxiaobleach <1241368737@qq.com>
2023-12-29 13:42:48 +0800

    update

--- backend/groth16/bw6-761/icicle/icicle.go
+++ backend/groth16/bw6-761/icicle/icicle.go
@@ -359,6 +359,7 @@ func Prove(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, opts ...b
 			if krs2, _, err = iciclegnark.MsmOnDevice(h, pk.G1Device.Z, sizeH, true); err != nil {
 				return err
 			}
+			fmt.Printf("icicleRes == krs2: %+v \n", krs2_cpu.Equal(&krs2))
 		}
 
 		// filter the wire values if needed
