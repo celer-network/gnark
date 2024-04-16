9392c813cb3f313599a4e362d147d289d65e8ca9
liuxiaobleach <1241368737@qq.com>
2024-04-16 16:09:05 +0800

    update

---
+++ testgpu/large_circuit_test.go
@@ -0,0 +1,59 @@
+package testgpu
+
+import (
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/backend/groth16"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/frontend/cs/r1cs"
+	"github.com/consensys/gnark/logger"
+	"github.com/consensys/gnark/test"
+	"github.com/rs/zerolog"
+	"os"
+	"testing"
+)
+
+type LargeCircuitCommitment struct {
+	P, Q frontend.Variable
+	N    frontend.Variable `gnark:",public"`
+}
+
+func TestLargeCircuitInGpu(t *testing.T) {
+	logger.Set(zerolog.New(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: "15:04:05"}).With().Timestamp().Logger())
+	assert := test.NewAssert(t)
+
+	field := ecc.BN254.ScalarField()
+
+	innerCcs, err := frontend.Compile(field, r1cs.NewBuilder, &LargeCircuitCommitment{})
+	assert.NoError(err)
+	innerPK, innerVK, err := groth16.Setup(innerCcs)
+	assert.NoError(err)
+
+	// inner proof
+	innerAssignment := &LargeCircuitCommitment{
+		P: 3,
+		Q: 5,
+		N: 15,
+	}
+	innerWitness, err := frontend.NewWitness(innerAssignment, field)
+	assert.NoError(err)
+	innerProof, err := groth16.Prove(innerCcs, innerPK, innerWitness)
+	assert.NoError(err)
+	innerPubWitness, err := innerWitness.Public()
+	assert.NoError(err)
+	err = groth16.Verify(innerProof, innerVK, innerPubWitness)
+	assert.NoError(err)
+}
+
+func (c *LargeCircuitCommitment) Define(api frontend.API) error {
+	res := api.Mul(c.P, c.Q)
+	api.AssertIsEqual(res, c.N)
+
+	commitment, err := api.Compiler().(frontend.Committer).Commit(c.P, c.Q, c.N)
+	if err != nil {
+		return err
+	}
+
+	api.AssertIsDifferent(commitment, 0)
+
+	return nil
+}
