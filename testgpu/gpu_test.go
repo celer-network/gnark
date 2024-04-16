c08a4794bc95a59cb1cf9a6c1ea495757ea66d90
liuxiaobleach <1241368737@qq.com>
2024-04-16 11:16:39 +0800

    add loop test

--- testgpu/gpu_test.go
+++ testgpu/gpu_test.go
@@ -80,6 +80,9 @@ func TestBn254VerifyBw6761(t *testing.T) {
 	assert.NoError(err)
 	outerWitness, err := frontend.NewWitness(assigment, ecc.BN254.ScalarField())
 	assert.NoError(err)
+	for i := 0; i < 2; i++ {
+		groth16.Prove(outerCcs, outerPK, outerWitness, backend.WithIcicleAcceleration())
+	}
 	outerProof, err := groth16.Prove(outerCcs, outerPK, outerWitness, backend.WithIcicleAcceleration())
 	assert.NoError(err)
 	outerPubWitness, err := outerWitness.Public()
