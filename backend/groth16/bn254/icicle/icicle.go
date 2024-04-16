41e469a152d9b4de16021302b6ffd281c4e39161
liuxiaobleach <1241368737@qq.com>
2024-04-16 16:31:03 +0800

    update

--- backend/groth16/bn254/icicle/icicle.go
+++ backend/groth16/bn254/icicle/icicle.go
@@ -200,7 +200,7 @@ func Prove(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, opts ...b
 	h_device := computeHonDevice(solution.A, solution.B, solution.C, &pk.Domain, stream)
 
 	// cpu calculate h
-	var h []fr.Element
+	/*var h []fr.Element
 	chHDone := make(chan struct{}, 1)
 	go func() {
 		h = computeH(solution.A, solution.B, solution.C, &pk.Domain)
@@ -208,7 +208,7 @@ func Prove(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, opts ...b
 		solution.B = nil
 		solution.C = nil
 		chHDone <- struct{}{}
-	}()
+	}()*/
 
 	// we need to copy and filter the wireValues for each multi exp
 	// as pk.G1.A, pk.G1.B and pk.G2.B may have (a significant) number of point at infinity
@@ -307,7 +307,7 @@ func Prove(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, opts ...b
 	ar.AddMixed(&deltas[0])
 	proof.Ar.FromJacobian(&ar)
 
-	<-chHDone
+	//<-chHDone
 
 	var krs, krs2, p1 curve.G1Jac
 	gerr = bn254.Msm(h_device, pk.G1Device.Z, &cfg, out)
@@ -321,14 +321,14 @@ func Prove(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, opts ...b
 	solution.B = nil
 	solution.C = nil
 
-	var cpuKrs2 curve.G1Jac
+	/*var cpuKrs2 curve.G1Jac
 	sizeH := int(pk.Domain.Cardinality - 1)
 	_, err = cpuKrs2.MultiExp(pk.G1.Z, h[:sizeH], ecc.MultiExpConfig{NbTasks: n / 2})
 	if err != nil {
 		return nil, fmt.Errorf("error in cpu MultiExp cpuKrs2: %v", err)
-	}
+	}*/
 	krs2 = *iciclegnark.G1ProjectivePointToGnarkJac(&outHost[0])
-	lg.Debug().Msg(fmt.Sprintf("gpu ar equal cpu krs2: %v", cpuKrs2.Equal(&krs2)))
+	//lg.Debug().Msg(fmt.Sprintf("gpu ar equal cpu krs2: %v", cpuKrs2.Equal(&krs2)))
 
 	// filter the wire values if needed
 	// TODO Perf @Tabaie worst memory allocation offender
