20530c7d8ab15b9d13b34335b04d2cb7c850eefe
liuxiaobleach <1241368737@qq.com>
2024-05-09 20:18:27 +0800

    update

--- testgpu/large_circuit_test.go
+++ testgpu/large_circuit_test.go
@@ -8,7 +8,9 @@ import (
 	"github.com/consensys/gnark/backend"
 	"github.com/consensys/gnark/backend/groth16"
 	icicle_bls12377 "github.com/consensys/gnark/backend/groth16/bls12-377/icicle"
+	icicle_bw6761 "github.com/consensys/gnark/backend/groth16/bw6-761/icicle"
 	cs_bls12377 "github.com/consensys/gnark/constraint/bls12-377"
+	cs_bw6761 "github.com/consensys/gnark/constraint/bw6-761"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/frontend/cs/r1cs"
 	"github.com/consensys/gnark/logger"
@@ -72,4 +74,39 @@ func (c *LargeCircuitCommitment) Define(api frontend.API) error {
 	api.AssertIsDifferent(commitment, 0)
 
 	return nil
+}
+
+func TestLargeCircuitInGpuOnBw6761(t *testing.T) {
+	logger.Set(zerolog.New(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: "15:04:05"}).With().Timestamp().Logger())
+	assert := test.NewAssert(t)
+
+	field := ecc.BW6_761.ScalarField()
+	var p, q [TEST_SIZE]frontend.Variable
+	for i := 0; i < TEST_SIZE; i++ {
+		p[i] = 3
+		q[i] = 5
+	}
+	innerCcs, err := frontend.Compile(field, r1cs.NewBuilder, &LargeCircuitCommitment{
+		P: p,
+		Q: q,
+	})
+	assert.NoError(err)
+	innerPK, innerVK, err := groth16.Setup(innerCcs)
+	assert.NoError(err)
+	// inner proof
+	innerAssignment := &LargeCircuitCommitment{
+		P: p,
+		Q: q,
+		N: 15,
+	}
+	innerWitness, err := frontend.NewWitness(innerAssignment, field)
+	assert.NoError(err)
+	innerPubWitness, err := innerWitness.Public()
+	assert.NoError(err)
+	for i := 0; i < 10; i++ {
+		innerProof, err := icicle_bw6761.Prove(innerCcs.(*cs_bw6761.R1CS), innerPK.(*icicle_bw6761.ProvingKey), innerWitness, backend.WithIcicleAcceleration())
+		assert.NoError(err)
+		err = groth16.Verify(innerProof, innerVK, innerPubWitness)
+		assert.NoError(err)
+	}
 }
