422a7c9529131c0b1327bef83945338931a1917f
liuxiaobleach <1241368737@qq.com>
2024-05-16 21:02:48 +0800

    update

--- backend/groth16/bls12-377/icicle/icicle_multi.go
+++ backend/groth16/bls12-377/icicle/icicle_multi.go
@@ -161,6 +161,7 @@ func (pk *ProvingKey) setupDevicePointersOnMulti(deviceIds []int, freePk bool) e
 
 // Prove generates the proof of knowledge of a r1cs with full witness (secret + public part).
 func ProveOnMulti(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, opts ...backend.ProverOption) (*groth16_bls12377.Proof, error) {
+	solveLimit <- 1
 	log := logger.Logger().With().Str("curve", r1cs.CurveID().String()).Str("acceleration", "icicle").Int("nbConstraints", r1cs.GetNbConstraints()).Str("backend", "groth16").Logger()
 	log.Debug().Msg("start ProveOnMulti")
 	opt, err := backend.NewProverConfig(opts...)
@@ -217,7 +218,6 @@ func ProveOnMulti(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, op
 		return nil
 	}))
 
-	solveLimit <- 1
 	_solution, err := r1cs.Solve(fullWitness, solverOpts...)
 	<-solveLimit
 	if err != nil {
