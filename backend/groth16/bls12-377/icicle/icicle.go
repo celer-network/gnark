c0577f636fc2b8cb0b025efc5f10bc82226fdd65
liuxiaobleach <1241368737@qq.com>
2024-05-17 10:43:33 +0800

    update

--- backend/groth16/bls12-377/icicle/icicle.go
+++ backend/groth16/bls12-377/icicle/icicle.go
@@ -43,7 +43,7 @@ const HasIcicle = true
 var (
 	singleDeviceLock sync.Mutex
 	deviceSetupLock  sync.Mutex
-	solveLimit       = make(chan int, 6)
+	solveLimit       = make(chan int, 4)
 )
 
 type deviceInfo struct {
