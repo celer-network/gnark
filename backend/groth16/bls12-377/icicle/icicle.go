c111a29615bb5d482442dadbed6adcb79ea1b499
liuxiaobleach <1241368737@qq.com>
2024-05-22 15:04:55 +0800

    update

--- backend/groth16/bls12-377/icicle/icicle.go
+++ backend/groth16/bls12-377/icicle/icicle.go
@@ -289,9 +289,9 @@ func Prove(r1cs *cs.R1CS, pk *ProvingKey, fullWitness witness.Witness, opts ...b
 		return nil
 	}))
 
-	solveLimit <- 1
+	SolveLimit <- 1
 	_solution, err := r1cs.Solve(fullWitness, solverOpts...)
-	<-solveLimit
+	<-SolveLimit
 	if err != nil {
 		return nil, err
 	}
